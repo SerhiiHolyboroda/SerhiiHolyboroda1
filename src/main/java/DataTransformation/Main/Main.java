784341a301beaa38e7c677b76bdc6cf250c5574a
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2021-02-17 19:40:44 +0200

    adding changes

--- src/main/java/DataTransformation/Main/Main.java
+++ src/main/java/DataTransformation/Main/Main.java
@@ -1,15 +1,16 @@
 package DataTransformation.Main;
 
-import com.DataTransformation.Classes.FileTransformer;
+import DataTransformation.Classes.FileCreation;
+
 import com.opencsv.exceptions.CsvException;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, CsvException {
-//        FileCreation a = new FileCreation();
-//        a.createCSV("Random");
-        FileTransformer f = new FileTransformer();
-        f.transformData();
+        FileCreation a = new FileCreation();
+        a.createCSV("Random");
+//        FileTransformer f = new FileTransformer();
+//        f.transformData();
     }
 }
